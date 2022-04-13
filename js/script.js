1b841433d2f301c19a5f2092f47aacda534e8237
iuripaiva27@gmail.com <iuripaiva27@gmail.com>
2022-04-13 14:27:06 -0300

    Separated JS from HTML

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var binary = "";
+
+		function convert() {
+
+
+			//convert the variable 'binary' to a array
+			var binaryArray = binary.split("", binary.length);
+	
+			// Access and change binary variable
+			binary = String(document.getElementById("binary").value);
+			
+			// Convert binary value to decimal
+			var decimal = parseInt(binary, 2);
+
+			//check if the input value contains only 0s and 1s
+			var i = 0;
+			var charCounter = 0;
+
+			while (charCounter < binaryArray.length){
+				if (binaryArray[charCounter] != 0){
+					if (binaryArray[charCounter] != 1){
+						i++
+						charCounter++
+					} else {
+						charCounter++
+					}
+				}else{
+					charCounter++
+				}
+			}
+			if (i != 0){
+				document.getElementById("result").innerHTML = "Error! Type only 0 and 1.";
+			}else{
+				document.getElementById("result").innerHTML = decimal;
+			}
+
+			//timeout created to make it work in a single click
+			setTimeout(convert() , 20);
+		}
